f13f09005925d5fd1db17ec59c472e4bc10384c5
Dima Laptop <vavilov9823@gmail.com>
2022-03-25 15:52:19 +0200

    prepared folder for Hydrogen

--- assets/loading.js
+++
@@ -1,9 +0,0 @@
-const loading = document.getElementById("loading")
-
-function enableLoading(){
-	loading.classList.add("active")
-}
-
-function disableLoading(){
-	loading.classList.remove("active")
-}
